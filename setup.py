0204294adeef96004f51fb32722373bf6c21db89
Lakshman <lakshman14334@gmail.com>
2022-10-10 15:08:56 -0400

    setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="multi-reg",
+    version="0.0.1",
+    description="ML project",
+    author="Hanumandla Lakshman", 
+    packages=find_packages(),
+    license="MIT"
+)
